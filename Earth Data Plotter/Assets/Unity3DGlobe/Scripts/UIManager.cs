39399c25e562592caf800342e5c682941fc64ce9
zphill15 <designsargo@gmail.com>
2020-11-07 17:02:19 -0500

    Do it

---
+++ Earth Data Plotter/Assets/Unity3DGlobe/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject MainCanvas;
+    public Button Reset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset.onClick.AddListener(ResetScene);
+
+    }
+
+    void ResetScene()
+    {
+
+        SceneManager.LoadScene("WastePopulation");
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
