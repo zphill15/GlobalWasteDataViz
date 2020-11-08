a20a54044f8d794c5a59d8b30bf5ddc958617392
zphill15 <designsargo@gmail.com>
2020-11-08 10:28:56 -0500

    getting close
    new data added

--- Earth Data Plotter/Assets/Unity3DGlobe/Scripts/DataLoader.cs
+++ Earth Data Plotter/Assets/Unity3DGlobe/Scripts/DataLoader.cs
@@ -5,7 +5,7 @@ public class DataLoader : MonoBehaviour {
     public DataVisualizer Visualizer;
 	// Use this for initialization
 	void Start () {
-        TextAsset jsonData = Resources.Load<TextAsset>("population");
+        TextAsset jsonData = Resources.Load<TextAsset>("wastedata");
         string json = jsonData.text;
         SeriesArray data = JsonUtility.FromJson<SeriesArray>(json);
         Visualizer.CreateMeshes(data.AllData);
